e6b89121ecad75b18f6c0defa80be5e97615cbad
quifar <risqufaris@gmail.com>
2020-07-22 00:04:28 +0700

    Update web.php
    add some route

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,16 @@ Route::get('/', function () {
 
 Route::get('/home', function () {
     return view('home');
-});
+}) -> name('home');
+
+Route::get('/portofolio', function () {
+    return view('portofolio');
+}) -> name('portofolio');
+
+Route::get('/about', function () {
+    return view('about');
+}) -> name('about');
+
+Route::get('/contact', function () {
+    return view('contact');
+}) -> name('contact');
